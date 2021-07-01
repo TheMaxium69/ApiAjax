34000571f29915d2040118cf2a702e6959f0c237
TheMaxium69 <63310746+TheMaxium69@users.noreply.github.com>
2021-07-01 15:08:18 +0200

    Exo1 : Gene Blague

--- main.js
+++ main.js
@@ -1,2 +1,19 @@
-alert("cc");
 console.log("coucou");
+
+const myBtn = document.querySelector("#btn");
+const myP = document.querySelector("#p");
+
+myBtn.addEventListener('click', ()=>{
+
+    let myRequest = new XMLHttpRequest();
+    myRequest.open('GET', 'https://api.chucknorris.io/jokes/random');
+
+    myRequest.onreadystatechange = function() {
+        if (myRequest.readyState === 4) {
+          console.log(myRequest.responseText);
+          var reponse = JSON.parse(myRequest.responseText);
+          myP.innerHTML = reponse.value;
+        }
+      };
+    myRequest.send();
+} )
