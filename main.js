ee81a680f4897d0f3059fab0b9329248125632a0
TheMaxium69 <63310746+TheMaxium69@users.noreply.github.com>
2021-07-01 15:12:34 +0200

    simplification

--- main.js
+++ main.js
@@ -7,13 +7,10 @@ myBtn.addEventListener('click', ()=>{
 
     let myRequest = new XMLHttpRequest();
     myRequest.open('GET', 'https://api.chucknorris.io/jokes/random');
-
-    myRequest.onreadystatechange = function() {
-        if (myRequest.readyState === 4) {
+    myRequest.onload = () => {
           console.log(myRequest.responseText);
           var reponse = JSON.parse(myRequest.responseText);
           myP.innerHTML = reponse.value;
-        }
       };
     myRequest.send();
 } )
