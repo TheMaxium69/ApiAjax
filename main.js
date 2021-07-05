332eecff99ba9be97c0109322166710c09da4b62
TheMaxium69 <63310746+TheMaxium69@users.noreply.github.com>
2021-07-05 09:37:48 +0200

    Connect PhpGateau

--- main.js
+++ main.js
@@ -1,23 +1,29 @@
-console.log("coucou");
-
+console.log("JavaScript Charged");
 const myBtn = document.querySelector("#btn");
 const myP = document.querySelector("#p");
 
 myBtn.addEventListener('click', ()=>{
 
     let myRequest = new XMLHttpRequest();
-    myRequest.open('GET', 'https://api.chucknorris.io/jokes/random');
-    myRequest.onload = () => {
-          console.log(myRequest.responseText);
-          var reponse = JSON.parse(myRequest.responseText);
-          myP.innerHTML = reponse.value;
+    // myRequest.open('GET', 'https://api.chucknorris.io/jokes/random');
+    // myRequest.onload = () => {
+    //       console.log(myRequest.responseText);
+    //       var reponse = JSON.parse(myRequest.responseText);
+    //       myP.innerHTML = reponse.value;
         
-          let dataTemp = reponse.value;
+    //       let dataTemp = reponse.value;
 
-          myP.innerHTML = "<hr>" + myP.innerHTML+ "/" + dataTemp; 
+    //       myP.innerHTML = "<hr>" + myP.innerHTML+ "/" + dataTemp; 
 
-
-
+    myRequest.open('GET', 'http://localhost/PhpGateau/index.php?controller=gateau&task=indexApi');
+    myRequest.onload = () => {
+    
+        
+      console.log(myRequest.responseText);
+      myP.innerHTML = myRequest.responseText;
+      
+           var reponse = JSON.parse(myRequest.responseText);
+           console.log(reponse['0']);
 
 
 
