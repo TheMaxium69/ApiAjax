eeed8c98c6e938798ade2b68d1c41ba2e565fbf2
TheMaxium69 <63310746+TheMaxium69@users.noreply.github.com>
2021-07-05 13:39:04 +0200

    update

--- main.js
+++ main.js
@@ -1,58 +1,46 @@
 console.log("JavaScript Charged");
+
 const myBtn = document.querySelector("#btn");
 const myP = document.querySelector("#p");
-
 myBtn.addEventListener('click', ()=>{
 
     let myRequest = new XMLHttpRequest();
-    // myRequest.open('GET', 'https://api.chucknorris.io/jokes/random');
-    // myRequest.onload = () => {
-    //       console.log(myRequest.responseText);
-    //       var reponse = JSON.parse(myRequest.responseText);
-    //       myP.innerHTML = reponse.value;
-        
-    //       let dataTemp = reponse.value;
-
-    //       myP.innerHTML = "<hr>" + myP.innerHTML+ "/" + dataTemp; 
-
     myRequest.open('GET', 'http://localhost/PhpGateau/index.php?controller=gateau&task=indexApi');
     myRequest.onload = () => {
-    
+        var reponse = JSON.parse(myRequest.responseText);
         
-      console.log(myRequest.responseText);
-      
-           var reponse = JSON.parse(myRequest.responseText);
-           console.log(reponse);
-           console.log(reponse['0']);
-           myP.innerHTML = "name : " + reponse['0'].name + "<br> gout : " + reponse['0'].gout;
-
+        console.log("---------------INDEXAPI------------");
         reponse.forEach(gateau => {
             console.log("name : " + gateau.name + " | gout : " + gateau.gout);
         });
-
       };
     myRequest.send();
 } )
 
-const myBtn2 = document.querySelector("#Secondbtn");
+const myBtn2 = document.querySelector("#btn2");
 const myP2 = document.querySelector("#p2");
+
+const myBtn3 = document.querySelector("#btn3");
+const myP3 = document.querySelector("#p3");
+
+//inutile FormData
+var data = new FormData();
+data.append('id', '3');
 
 myBtn2.addEventListener('click', ()=>{
 
     let myRequest = new XMLHttpRequest();
-    myRequest.open('GET', 'http://localhost/PhpGateau/index.php?controller=gateau&task=showApi&id=1');
+    myRequest.open('POST', 'http://localhost/PhpGateau/index.php?controller=gateau&task=showApi');
     myRequest.onload = () => {
-    
-        
       var reponse = JSON.parse(myRequest.responseText);
-      
-      console.log("GATEAUX");
-      console.log("name : " + reponse.gateau.name + " | gout : " + reponse.gateau.gout);
-      console.log("RECIPE");
-      reponse.recipes.forEach(recipe => {
-        console.log("name : " + recipe.name + " | desc : " + recipe.gout);
-      })
-
+        
+        console.log("---------------SHOWAPI------------");  
+        console.log("GATEAUX");
+        console.log("name : " + reponse.gateau.name + " | gout : " + reponse.gateau.gout);
+        console.log("RECETTE");
+        reponse.recipes.forEach(recipe => {
+          console.log("name : " + recipe.name + " | desc : " + recipe.desc);
+        })
       };
-    myRequest.send();
-} )
+    myRequest.send('id=3');
+})
