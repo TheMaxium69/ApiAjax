8b6eb38a311e640aa71e9fb5d8132927acad9857
TheMaxium69 <63310746+TheMaxium69@users.noreply.github.com>
2021-07-05 10:10:53 +0200

    show and index

--- main.js
+++ main.js
@@ -20,12 +20,38 @@ myBtn.addEventListener('click', ()=>{
     
         
       console.log(myRequest.responseText);
-      myP.innerHTML = myRequest.responseText;
       
            var reponse = JSON.parse(myRequest.responseText);
+           console.log(reponse);
            console.log(reponse['0']);
+           myP.innerHTML = "name : " + reponse['0'].name + "<br> gout : " + reponse['0'].gout;
 
+        reponse.forEach(gateau => {
+            console.log("name : " + gateau.name + " | gout : " + gateau.gout);
+        });
 
+      };
+    myRequest.send();
+} )
+
+const myBtn2 = document.querySelector("#Secondbtn");
+const myP2 = document.querySelector("#p2");
+
+myBtn2.addEventListener('click', ()=>{
+
+    let myRequest = new XMLHttpRequest();
+    myRequest.open('GET', 'http://localhost/PhpGateau/index.php?controller=gateau&task=showApi&id=1');
+    myRequest.onload = () => {
+    
+        
+      var reponse = JSON.parse(myRequest.responseText);
+      
+      console.log("GATEAUX");
+      console.log("name : " + reponse.gateau.name + " | gout : " + reponse.gateau.gout);
+      console.log("RECIPE");
+      reponse.recipes.forEach(recipe => {
+        console.log("name : " + recipe.name + " | desc : " + recipe.gout);
+      })
 
       };
     myRequest.send();
